6960ac05475e72db9f25c90bb301d0f388eb4c45
robbytrujillo <robbyikusuma88@gmail.com>
2024-08-15 13:44:15 +0700

    helpdesk application

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -4,6 +4,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Dashboard extends CI_Controller {
     public function index() {
-        $this->load->view('back/dashboard');
+        $this->template->load('back/template', 'back/dashboard');
     }
 }
---
+++ application/views/back/template.php
@@ -0,0 +1,12 @@
+<?php $this->load->view('back/template/meta'); ?>
+    <div class="wrapper"></div>
+
+    <?php $this->load->view('back/template/header'); ?>
+    <?php $this->load->view('back/template/sidebar'); ?>
+
+    <?php echo $contents; ?>
+    <?php $this->load->view('back/template/footer'); ?>
+</div>
+<?php $this->load->view('back/template/script'); ?>
+    
+
