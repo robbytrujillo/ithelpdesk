91d559b7240d779e0664d14728a868017b7fae91
robbytrujillo <robbyikusuma88@gmail.com>
2024-08-21 05:47:28 +0700

    helpdesk application | function login

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -6,4 +6,8 @@ class Dashboard extends CI_Controller {
     public function index() {
         $this->template->load('back/template', 'back/dashboard');
     }
+
+    function login() {
+        
+    }
 }
