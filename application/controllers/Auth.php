bc979fae0cb5b9632674b0fd960813fc097addcd
robbytrujillo <robbyikusuma88@gmail.com>
2024-08-26 14:45:54 +0700

    insert register ke database

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -20,5 +20,27 @@ class Auth extends CI_Controller {
         $this->form_validation->set_rules('email', 'Email', 'trim|required');
         $this->form_validation->set_rules('password', 'Password', 'trim|required');
         $this->form_validation->set_rules('confirm_password', 'Confirm password', 'trim|required');
+
+        $this->form_validation->set_message('required','{field} Harus diisi');
+
+        $this->form_validation->set_error_delimeters('<div class="alert alert-danger">','</div>');
+
+        if ($this->form_validation->run() == TRUE) {
+            $data = array(
+                'username'=> $this->input->post('username'),
+                'email'=> $this->input->post('email'),
+                'password'=> $this->input->post('password'),
+                'status_user' => 1,
+                'level_user' => 1,
+            );
+
+            $this->M_auth->insert($data);
+
+            $this->session->set_flashdata('message','<div class="alert alert-info">Data Berhasil Di Simpan</div>');
+
+            redirect('auth/login','refresh')
+        } else {
+            # code
+        }
     }
 }
