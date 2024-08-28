e4397d7101cf1f329ebdceada09541ac99b263c5
robbytrujillo <robbyikusuma88@gmail.com>
2024-08-28 13:15:32 +0700

    add validation email not valid and unique | Register Process

--- application/views/back/register.php
+++ application/views/back/register.php
@@ -28,7 +28,9 @@
   <div class="card">
     <div class="card-body register-card-body" style="border-radius: 90%">
       <p class="login-box-msg">Register a new membership</p>
-
+      
+      <?= $this->session->flashdata('message'); ?>
+      <?php validation_errors() ?>
       <form action="<?= base_url('auth/proses_register') ?>" method="post">
         <div class="input-group mb-3">
           <input type="text" name="username" class="form-control" placeholder="Username">
