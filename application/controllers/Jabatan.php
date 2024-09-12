6667e7946f9ace952a97d10cd76b4d840b234a72
robbytrujillo <robbyikusuma88@gmail.com>
2024-09-12 09:49:01 +0700

    add Data Jabatan to database processing

--- application/controllers/Jabatan.php
+++ application/controllers/Jabatan.php
@@ -5,9 +5,30 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Jabatan extends CI_Controller {
     public function index() {
         // $this->load->model('M_jabatan');
-
         $data['jabatan'] = $this->M_jabatan->get_jabatan();
+
         $this->template->load('back/template', 'back/jabatan/jabatan', $data);
+    }
+
+    function save_jabatan() {
+        $this->form_validation->set_rules('jabatan', 'Jabatan', 'trim|required');
+        
+        $this->form_validation->set_message('required','{field} Harus diisi');
+
+        $this->form_validation->set_error_delimiters('<div class="alert alert-danger">','</div>');
+
+        if ($this->form_validation->run() == TRUE) {
+            $data = [
+                'jabatan' => $this->input->post('jabatan'),
+            ];
+
+            $this->M_jabatan->insert($data);
+            $this->session->set_flashdata('message','<div class="alert alert-info">Data Berhasil Di Simpan</div>');
+
+            redirect('jabatan','refresh');
+        } else {
+            $this->index();
+        }
     }
 }
 
--- application/models/M_jabatan.php
+++ application/models/M_jabatan.php
@@ -6,4 +6,8 @@ class M_jabatan extends CI_Model {
     function get_jabatan() {
         return $this->db->get('jabatan')->result();
     }
+
+    function insert($data) {
+        return $this->db->insert('jabatan', $data);
+    }
 }
