bb2862950e323ba224b7c7faf1e9a4c6bddcdb15
robbytrujillo <robbyikusuma88@gmail.com>
2024-09-12 09:22:26 +0700

    add view Data Jabatan

--- application/views/back/jabatan/jabatan.php
+++ application/views/back/jabatan/jabatan.php
@@ -8,7 +8,24 @@
     </section>
     <section class="content">
         <div class="row mt-2">
-            <div class="col-12">
+            <div class="col-6">
+            <div class="card">
+                    <div class="card-header">
+                        <h3 class="card-title">Tambah Jabatan</h3>
+                    </div>
+                    <div class="card-body">
+                        <form action="<?= base_url('jabatan/save_jabatan') ?>" method="POST">
+                            <div class="form-group">
+                                <label>Jabatan</label>
+                                <input type="text" name="jabatan" class="form-control">
+                            </div>
+                            <button type="submit" class="btn btn-primary btn-sm"> Save </button>
+                            <button type="reset" class="btn btn-danger btn-sm"> Reset </button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <div class="col-6">
                 <div class="card">
                     <div class="card-header">
                         <h3 class="card-title">Data Jabatan</h3>
@@ -23,7 +40,7 @@
                                 </tr>
                             </thead>
                             <tbody>
-                                <?php 
+                            <?php 
                                 $no = 1;
                                 foreach($jabatan as $row) { ?>
                                 <tr>
