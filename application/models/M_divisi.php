feb70711f67bb4099c32f0bf1648a0906d09a051
robbytrujillo <robbyikusuma88@gmail.com>
2024-09-22 06:25:33 +0700

    divisi data view

--- application/models/M_divisi.php
+++ application/models/M_divisi.php
@@ -5,6 +5,10 @@ class M_divisi extends CI_Model {
     public function get_divisi() {
         return $this->db->get('divisi')->result();
     }
+
+    function insert($data) {
+        return $this->db->insert('jabatan', $data);
+    }
 }
 
 
