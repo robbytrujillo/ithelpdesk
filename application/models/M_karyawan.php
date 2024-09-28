ddc8f5943978d3a0ebe737331bfbb22546ae0e1f
robbytrujillo <robbyikusuma88@gmail.com>
2024-09-28 11:42:03 +0700

    karyawan data view | model karyawan

--- application/models/M_karyawan.php
+++ application/models/M_karyawan.php
@@ -3,5 +3,7 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class M_karyawan extends CI_Model {
-    
+    function get_karyawan() {
+        return $this->db->get('users')->result();
+    }
 }
