3216341dc40d88d65c7d0aaeba77fbafa9314b99
robbytrujillo <robbyikusuma88@gmail.com>
2024-10-05 18:30:59 +0700

    karyawan data view | form edit karyawan

--- application/controllers/Karyawan.php
+++ application/controllers/Karyawan.php
@@ -67,4 +67,40 @@ class Karyawan extends CI_Controller {
             redirect('karyawan','refresh');
         }
     }
+
+    function update_karyawan() {
+        $this->form_validation->set_rules('nik', 'NIK', 'trim|is_unique[users.nik]|required');
+        $this->form_validation->set_rules('username', 'Username', 'trim|required');
+        $this->form_validation->set_rules('email', 'Email', 'valid_email|is_unique[users.email]|required');
+        $this->form_validation->set_rules('password', 'Password', 'trim|min_length[5]|required');
+        $this->form_validation->set_rules('confirm_password', 'Confirm Password', 'trim|matches[password]|required');
+
+        $this->form_validation->set_message('required','{field} Harus diisi');
+        $this->form_validation->set_message('valid_email','{field} anda harus valid');
+
+        $this->form_validation->set_error_delimiters('<div class="alert alert-danger">','</div>');
+
+        if ($this->form_validation->run() == TRUE) {
+            $data = array(
+                'nik'=> $this->input->post('nik'),
+                'username'=> $this->input->post('username'),
+                'email'=> $this->input->post('email'),
+                'jabatan_id'=> $this->input->post('jabatan_id'),
+                'divisi_id'=> $this->input->post('divisi_id'),
+                'password'=> password_hash($this->input->post('password'), PASSWORD_BCRYPT),
+                'status_user' => 1,
+                'level_user' => 1,
+            );
+
+            // var_dump($data);
+
+            $this->M_karyawan->insert($data);
+
+            $this->session->set_flashdata('message','<div class="alert alert-info">Data Berhasil Di Simpan</div>');
+
+            redirect('karyawan','refresh');
+        } else {
+            $this->add_karyawan();
+        }
+    }
 }
