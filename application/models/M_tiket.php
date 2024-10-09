c79aa45965d138dd0e93097f9d9bee387a8ba4cf
robbytrujillo <robbyikusuma88@gmail.com>
2024-10-09 08:48:32 +0700

    detail tiket data view

--- application/models/M_tiket.php
+++ application/models/M_tiket.php
@@ -10,6 +10,8 @@ class M_tiket extends CI_Model {
     function get_no_tiket($no_tiket) {
         // $this->db->where('id_users', $no_tiket);
         $this->db->join('users', 'tiket.user_id = users.id_users', 'left');
+        $this->db->join('divisi', 'users.divisi_id = divisi.id_divisi', 'left');
+        $this->db->join('jabatan', 'users.jabatan_id = jabatan.id_jabatan', 'left');
         $this->db->where('no_tiket', $no_tiket);
 
         return $this->db->get('tiket')->row();
