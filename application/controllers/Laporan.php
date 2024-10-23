493a1151670858aa91848d2c375f6c1e0039ae96
robbytrujillo <robbyikusuma88@gmail.com>
2024-10-23 22:24:45 +0700

    view laporan

--- application/controllers/Laporan.php
+++ application/controllers/Laporan.php
@@ -4,6 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Laporan extends CI_Controller {
     public function index() {
-        
+        $data['title'] = 'Laporan';
+        $this->template->load('back/template', 'back/laporan/laporan', $data);
     }
 }
