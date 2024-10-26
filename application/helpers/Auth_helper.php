64b740e608008abb9e0caffdaa79a7afa516fdc3
robbytrujillo <robbyikusuma88@gmail.com>
2024-10-26 10:25:23 +0700

    Proteksi Halaman Dashboard

---
+++ application/helpers/Auth_helper.php
@@ -0,0 +1,13 @@
+<?php 
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+function cek_login() {
+    $CI = &get_instance();
+    $email = $CI->session->email;
+
+    if ($email == NULL) {
+        $CI->session->set_flashdata('message', '<div class="alert alert-danger"> Harus Login Dulu! </div>');
+        redirect('login');
+    }
+}
